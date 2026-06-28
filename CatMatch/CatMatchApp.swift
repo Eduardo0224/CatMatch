b1d397efe04cc4ecb3e884aa14bc7821bbde1469
Eduardo Andrade <eduardrade0224@gmail.com>
2026-06-28 08:10:02 -0500

    Initial Commit

---
+++ CatMatch/CatMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatMatchApp.swift
+//  CatMatch
+//
+//  Created by Eduardo Andrade on 28/06/26.
+//
+
+import SwiftUI
+
+@main
+struct CatMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatMatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CatMatch
+//
+//  Created by Eduardo Andrade on 28/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
